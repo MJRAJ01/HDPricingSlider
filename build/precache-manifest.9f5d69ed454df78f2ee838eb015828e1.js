3ddb89472a2cf5081040b38c7b6d47ffd931eb76
MJRAJ01 <61257131+MJRAJ01@users.noreply.github.com>
2020-08-13 14:58:12 -0400

    Build version

---
+++ build/precache-manifest.9f5d69ed454df78f2ee838eb015828e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e536c9cff02a39cbd6df79a4327890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26dda655218a0861b4f5",
+    "url": "/static/css/main.bebb6fac.chunk.css"
+  },
+  {
+    "revision": "0e9bc0986a32834066b5",
+    "url": "/static/js/2.e23f0133.chunk.js"
+  },
+  {
+    "revision": "26dda655218a0861b4f5",
+    "url": "/static/js/main.88661e95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
